c806c493df31e3b87120821d160d8d76bf54938d
Aryan Katakwar <arykat18@gmail.com>
2025-07-28 22:01:13 -0400

    Setup Firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAlk33IJDi4m8CGfZwZNYDY9OaAvtSeEs",
+  authDomain: "musiclov-d0e4b.firebaseapp.com",
+  projectId: "musiclov-d0e4b",
+  storageBucket: "musiclov-d0e4b.firebasestorage.app",
+  messagingSenderId: "331124210976",
+  appId: "1:331124210976:web:69bfb646449a508ede6b45",
+  measurementId: "G-EREBKJS2PY"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
+
